75a2875f87c0dd1ca7999f1c9c7ff08b881db45d
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-12-17 00:30:01 -0500

    layout complete

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(() => {
   var now = dayjs().format("dddd, MMMM DD, YYYY");
   $("#currentDay").append(now);
+
+  var APIKey = "beefdab294cbf81ff26e12192d4e7200";
+
+  // Here we are building the URL we need to query the database
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=Bujumbura,Burundi&appid=" +
+    APIKey;
 });
