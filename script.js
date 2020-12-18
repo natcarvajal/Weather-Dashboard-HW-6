671f6a95a644baff7c5522e673ba59e4954c7cd0
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-12-18 02:32:02 -0500

    sqaure one

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-$(document).ready(() => {
+$(document).ready(function () {
+  var city = $("#inputValue");
   var APIKey = "beefdab294cbf81ff26e12192d4e7200";
-
-  // Here we are building the URL we need to query the database
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Bujumbura,Burundi&appid=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=München,DE&units=imperial&appid=" +
     APIKey;
+  // var one_call = "https://api.openweathermap.org/data/2.5/onecall?";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then((response) => {
+    console.log(response);
+  });
 });
