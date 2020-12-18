3fe67b1cfda78a4eb5a621d361ce1808cb7f5122
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-12-18 02:49:01 -0500

    adding display and temp

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  var city = $("#inputValue");
   var APIKey = "beefdab294cbf81ff26e12192d4e7200";
   var queryURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=München,DE&units=imperial&appid=" +
@@ -10,5 +9,8 @@ $(document).ready(function () {
     method: "GET",
   }).then((response) => {
     console.log(response);
+    $(".display-4").append(response.city.name);
+    $(".temperature").append(response.list[0].main.temp);
+    $(".temperature").append(response.list[0].main.temp);
   });
 });
