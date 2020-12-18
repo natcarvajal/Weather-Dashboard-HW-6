0d8e3d15d455a901516889517fe6ae8f89d39ada
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-12-18 12:49:01 -0500

    api works

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
-  var cityName = $("#inputValue");
-  var APIKey = "beefdab294cbf81ff26e12192d4e7200";
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=München,DE&units=imperial&appid=" +
-    APIKey;
-  var one_call =
-    "http://api.openweathermap.org/data/2.5/uvi?lat=48.1374&lon=11.5755&appid=" +
-    APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then((response) => {
-    console.log(response);
-    $(" .display-4").append(response.city.name);
-    $(" .temperature").append(response.list[0].main.temp);
-    $(" .humidity").append(response.list[0].main.humidity);
-    $(" .windspeed").append(response.list[0].wind.speed);
-  });
-  $.ajax({
-    url: one_call,
-    method: "GET",
-  }).then((response) => {
-    console.log(response);
-    $(" .uv-index").append(response.value);
+  $("#button").on("click", function (event) {
+    event.preventDefault();
+    var cityName = $("#inputValue").val();
+    var APIKey = "beefdab294cbf81ff26e12192d4e7200";
+    var one_call =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=48.1374&lon=11.5755&appid=" +
+      APIKey;
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityName +
+        "&units=imperial&appid=" +
+        APIKey,
+      method: "GET",
+    }).then((response) => {
+      console.log(response);
+      $(".display-4").append(response.city.name);
+      $(".temperature").append(response.list[0].main.temp);
+      $(".humidity").append(response.list[0].main.humidity);
+      $(".windspeed").append(response.list[0].wind.speed);
+    });
+
+    $.ajax({
+      url: one_call,
+      method: "GET",
+    }).then((response) => {
+      console.log(response);
+      $(".uv-index").append(response.value);
+    });
   });
 });
