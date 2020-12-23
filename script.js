66e47a6e0bf98a97808de473ec83b67980da4f14
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2020-12-23 11:28:50 -0500

    able to add buttons of previous cities searched

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  var cities = ["New York City", "Madrid", "Rome", "Los Angeles", "Sydney"];
+  var cities = [];
   function weatherInfo() {
     var city = $(this).attr("data-name");
     var APIKey = "beefdab294cbf81ff26e12192d4e7200";
     $.ajax({
       url:
         "https://api.openweathermap.org/data/2.5/forecast?q=" +
-        cityName +
+        city +
         "&units=imperial&appid=" +
         APIKey,
       method: "GET",
@@ -37,19 +37,14 @@ $(document).ready(function () {
   function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < cities.length; i++) {
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var b = $("<button>");
-      // Adding a class of movie-btn to our button
       b.addClass("city-btn");
-      // Adding a data-attribute
       b.attr("data-name", cities[i]);
-      // Providing the initial button text
       b.text(cities[i]);
-      // Adding the button to the buttons-view div
       $("#buttons-view").append(b);
     }
   }
+
   $("#add-city").on("click", function (event) {
     event.preventDefault();
     var city = $("#inputValue").val().trim();
